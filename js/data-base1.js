4b1c51f698d794dc7d0d46cb69d5e80b4c027bb3
Alvaro <alvaro-lopez_@hotmail.com>
2019-11-01 09:30:11 +0100

    link de cada id

--- js/data-base1.js
+++ js/data-base1.js
@@ -1,16 +1,64 @@
 // data-table      https://www.youtube.com/watch?v=70j_1YRfblM
 // fetch con data-table  https://datatables.net/forums/discussion/54076/use-fetch-api-instead-of-ajax-call-in-datatable
 $(document).ready(function() {
-    $('#output').DataTable({
+    $('#list-protest').dataTable({
         ajax: {
             url: "http://prueba-env.us-east-2.elasticbeanstalk.com/protests/list",
-            dataSrc: ""
+            dataSrc: "protests"
         },
-        columns: [{
-            data: "name",
-            render: function (data, type, name){
-                return "<a href='../protest-id" + name.id + "'>" + name.name + "</a>"
+        columns: [
+            {
+                data: "name",
+                render: function (data, type, name){
+                    return "<a href='../protest-id" + name.id + "'>" + name.name + "</a>"
+                },
+            },
+            {
+                data: "whoDefends",
+                render: function (data, type, whoDefends){
+                    return "<a href='../protest-id" + whoDefends.id + "'>" + whoDefends.whoDefends + "</a>"
+                },
+            },
+            {
+                data: "promotedBy",
+                render: function (data, type, promotedBy){
+                    return "<a href='../protest-id" + promotedBy.id + "'>" + promotedBy.promotedBy + "</a>"
+                },
+            },
+            {
+                data: "city",
+                render: function (data, type, city){
+                    return "<a href='../protest-id" + city.id + "'>" + city.city + "</a>"
+                },
+            },
+            {
+                data: "date",
+                render: function (data, type, date){
+                    return "<a href='../protest-id" + date.id + "'>" + date.date + "</a>"
+                },
+            },
+            {
+                data: "id",
+                render: function (deleteId){
+                    return "<button class='btn-link js-eliminar' data-name-id='" + deleteId + "'>Eliminar</button>"
+                },
             }
-        }]
+        ]
+    });
+
+    $("#list-protest").on("click","js-eliminar", function(){
+        var button = $(this);
+        bootbox.confirm("¿Esta seguro de eliminar?", function (result){
+            if (result){
+                $.ajax({
+                    url: "http://prueba-env.us-east-2.elasticbeanstalk.com/protests/list",
+                    method: "DELETE",
+                    success: function (){
+                        console.log("Petición Exitosa")
+                        button.parents("tr").remove();
+                    }
+                })
+            }
+        });
     });
 });
